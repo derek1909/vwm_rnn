1ecd3969f1a4401eb51440dd15124eaf1031dbd1
Derek cblgpu <jd976@cam.ac.uk>
2025-05-19 11:06:58 +0100

    Add gif analysis for error distribution

--- model_evolution_analysis.py
+++ model_evolution_analysis.py
@@ -10,12 +10,14 @@ import concurrent.futures
 from tqdm import tqdm
 from threading import Lock
 import ipdb
+import gc
 import warnings
 warnings.filterwarnings("ignore", message="You are using `torch.load` with `weights_only=False`")
 
 ## Example usage: 
 # (torch2) jd976@cblgpu07:/scratches/kolmogorov_2/jd976/working/vwm_rnn$ 
 # python model_evolution_analysis.py --config /homes/jd976/working/vwm_rnn/rnn_models/exp_RegStrength_MultiItem_wPoiNoiseHeterTau_longer/lambda_reg-0.10000000_n512item10PI1k0.005/config.yaml
+# python model_evolution_analysis.py --config /homes/jd976/working/vwm_rnn/rnn_models/PsychoSimil_4_2_n256item10PI1gamma0.05/config.yaml
 
 # Add project root to Python path
 sys.path.append(os.path.abspath("."))
@@ -26,14 +28,13 @@ from config import *
 from utils import generate_input
 
 # Set basic paths
-cuda_device = 0
 base_folder = os.path.dirname(config_path)
 model_folder = os.path.join(base_folder, "models")
 
 # ===== Option Settings =====
 plot_first_n = 250    # Only select first N models; 0 means no filtering.
 plot_step = 4       # Select every X-th model; 0 means no filtering.
-max_workers = 16
+max_workers = 1
 # ===========================
 
 def get_model_files():
@@ -147,6 +148,97 @@ def analyze_weights(model, iteration_num, save_folder):
         plt.close('all')
     return save_path
 
+torch.cuda.empty_cache()
+def analyze_error_dist(model, iteration_num, save_folder):
+    """
+    Plot the distribution of decoding errors for different item numbers after training.
+    This function visualizes the model's performance by comparing decoded angles with input angles.
+    """
+    num_trials = 6000
+    if max_item_num == 1:
+        item_num = [1]
+    elif max_item_num == 2:
+        item_num = [1,2]
+    elif max_item_num < 8:
+        item_num = list(range(1, max_item_num+1, 2))
+    else:
+        item_num = [8, 4, 2, 1]
+
+    # Split num_trials into len(item_num) groups
+    trials_per_group = num_trials // len(item_num)
+    remaining_trials = num_trials % len(item_num)
+    trial_counts = [trials_per_group + (1 if i < remaining_trials else 0) for i in range(len(item_num))]
+    
+    # Generate random presence indicators
+    # device = next(model.parameters()).device  # Use model device
+    input_presence = torch.zeros(num_trials, max_item_num, device=device, requires_grad=False)
+    start_index = 0
+    for i, count in enumerate(trial_counts):
+        end_index = start_index + count
+        one_hot_indices = torch.stack([
+            torch.randperm(max_item_num, device=device)[:item_num[i]] for _ in range(count)
+        ])
+        input_presence[start_index:end_index] = input_presence[start_index:end_index].scatter(1, one_hot_indices, 1)
+        start_index = end_index
+    
+    # Generate random input angles
+    input_thetas = (torch.rand(num_trials, max_item_num, device=device) * 2 * torch.pi) - torch.pi
+    
+    # Generate input tensor
+    u_t = generate_input(input_presence, input_thetas, input_strength, ILC_noise, T_init, T_stimi, T_delay, T_decode, dt)
+
+    # Run simulation and slice the output
+    r_output, _ = model(u_t, r0=None)  # (trial, steps, neuron)
+    step_threshold = int((T_init + T_stimi + T_delay) / dt)
+    r_decode = r_output[:, step_threshold:, :].transpose_(0, 1).clone()  # (steps_for_loss, trial, neuron)
+    del r_output    # delete r_output as it can be up to 8GB
+    torch.cuda.empty_cache()
+
+    # Decode the output
+    u_hat = model.readout(r_decode.reshape(-1, num_neurons)).reshape(r_decode.shape[0], num_trials, -1)
+    decoded_thetas =  model.decode(u_hat)  # (trials, max_items)
+    angular_diff = (input_thetas - decoded_thetas + torch.pi) % (2 * torch.pi) - torch.pi  # (trials,items)
+
+    with plot_lock:
+        # Plot error distribution
+        plt.figure(figsize=(6, 5))
+        x_values = np.linspace(-np.pi, np.pi, 100)
+        
+        start_index = 0
+        for i, count in enumerate(trial_counts):
+            end_index = start_index + count
+            mask = input_presence[start_index:end_index].bool()
+            sliced_angular_diff = angular_diff[start_index:end_index]
+            err = sliced_angular_diff[mask].detach().cpu().numpy()
+            
+            hist, bins = np.histogram(err, bins=x_values, density=True)
+            bin_centers = (bins[:-1] + bins[1:]) / 2
+            plt.plot(bin_centers, hist, label=f'{item_num[i]} item(s)')
+
+            start_index = end_index
+        
+        plt.xlim(-np.pi, np.pi)
+        plt.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi], 
+                [r'$-\pi$', r'$-\pi/2$', '0', r'$\pi/2$', r'$\pi$'])
+
+        plt.xlabel('Angular Error (radians)')
+        plt.ylabel('Probability Density')
+        plt.legend()
+        plt.title(f'Distribution of Decoding Errors (iteration {iteration_num})', fontsize=16)
+        plt.ylim(bottom=0)
+    
+        # Save the plot
+        save_path = os.path.join(save_folder, f'error_distrib_iteration{iteration_num}.png')
+        plt.savefig(save_path, dpi=200)
+        plt.close('all')
+
+    del input_presence, input_thetas, u_t
+    del r_decode, u_hat, decoded_thetas, angular_diff
+    torch.cuda.empty_cache()
+    gc.collect()
+    return save_path
+
+        
 # Dictionary to register analyses.
 # Each key is the analysis name, and the value is a dict with:
 #   - 'enabled': a boolean to toggle the analysis
@@ -155,18 +247,25 @@ def analyze_weights(model, iteration_num, save_folder):
 #   - 'gif_name': the name of the final GIF
 #   - 'requires_input': whether the analysis function needs the common input u_t.
 ANALYSES = {
-    # "activation": {
-    #     "enabled": True,
-    #     "function": analyze_activation,
-    #     "save_folder": os.path.join(model_folder, "ActivVsTau"),
-    #     "gif_name": "Activ_vs_Tau.gif",
-    #     "requires_input": True,
-    # },
+    "activation": {
+        "enabled": False,
+        "function": analyze_activation,
+        "save_folder": os.path.join(model_folder, "ActivVsTau"),
+        "gif_name": "Activ_vs_Tau.gif",
+        "requires_input": True,
+    },
     "weights": {
-        "enabled": True,
+        "enabled": False,
         "function": analyze_weights,
         "save_folder": os.path.join(model_folder, "WeightsEvolution"),
         "gif_name": "WeightsEvolution.gif",
+        "requires_input": False,
+    },
+    "error_dist": {
+        "enabled": True,
+        "function": analyze_error_dist,
+        "save_folder": os.path.join(model_folder, "ErrorDistEvolution"),
+        "gif_name": "ErrorDistEvolution.gif",
         "requires_input": False,
     },
 }
@@ -192,6 +291,7 @@ def create_common_input():
     u_t = generate_input(
         presence=input_presence,
         theta=input_thetas,
+        input_strength=input_strength,
         noise_level=ILC_noise,
         T_init=T_init,
         T_stimi=T_stimi,
@@ -207,9 +307,10 @@ def process_model_file(model_file, common_input):
     """
     iteration_num = int(re.findall(r'\d+', model_file)[0])
     model_path = os.path.join(model_folder, model_file)
-    model = RNNMemoryModel(max_item_num=max_item_num, num_neurons=num_neurons, dt=dt, tau_min=tau_min, tau_max=tau_max, device=device, positive_input=positive_input)
-    state_dict = torch.load(model_path, map_location=device)
-    model.load_state_dict(state_dict)
+    model = RNNMemoryModel(max_item_num, num_neurons, dt, tau_min, tau_max, spike_noise_type, 
+                           spike_noise_factor, saturation_firing_rate, device, positive_input, dales_law)
+    model.load_state_dict(torch.load(model_path, weights_only=False, map_location=device))
+    model.device = device
     model.eval()
 
     results = {}
@@ -230,9 +331,10 @@ def main():
     and then create GIFs for each analysis.
     """
     for key, analysis in ANALYSES.items():
-        os.makedirs(analysis["save_folder"], exist_ok=True)
+        if analysis["enabled"]:
+            os.makedirs(analysis["save_folder"], exist_ok=True)
 
-    common_input = create_common_input() if any(a["requires_input"] for a in ANALYSES.values()) else None
+    common_input = create_common_input() if any( (a["requires_input"] and a["enabled"]) for a in ANALYSES.values()) else None
     model_files = get_model_files()
 
     results = []
